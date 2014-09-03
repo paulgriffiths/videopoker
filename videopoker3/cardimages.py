a64f6617c83d141f2ddb7444ec6c047c9e73fad2
Paul Griffiths <mail@paulgriffiths.net>
2014-09-03 18:19:53 -0400

    Add large card images class.

--- videopoker3/cardimages.py
+++ videopoker3/cardimages.py
@@ -1,16 +1,16 @@
-"""Card images class."""
+"""Card images classes."""
 
-from tkinter import PhotoImage
+from PIL.ImageTk import PhotoImage
 
 
-class CardImages:
+class CardImagesSmall:
 
     '''Card images class.'''
 
     cardimgs = None
     backimg = None
     emptyimg = None
-    imgdir = "images/"
+    imgdir = "card_images_small/"
 
     def __init__(self):
 
@@ -45,6 +45,56 @@ class CardImages:
             cls.cardimgs = []
             for idx in range(1, 53):
                 newcard = PhotoImage(file="{0}{1}.gif".format(cls.imgdir, idx))
+                cls.cardimgs.append(newcard)
+
+        return cls.cardimgs[cardindex - 1]
+
+
+class CardImagesLarge:
+
+    '''Card images class.'''
+
+    cardimgs = None
+    backimg = None
+    emptyimg = None
+    imgdir = "card_images_large/"
+
+    def __init__(self):
+
+        '''This class is not intended to be instantiated.'''
+
+        pass
+
+    @classmethod
+    def empty(cls):
+
+        '''Returns an image for an empty card place.'''
+
+        if not cls.emptyimg:
+            cls.emptyimg = PhotoImage(file="{0}card_empty@2x.png".
+                                      format(cls.imgdir))
+        return cls.emptyimg
+
+    @classmethod
+    def back(cls):
+
+        '''Returns an image for the card back.'''
+
+        if not cls.backimg:
+            cls.backimg = PhotoImage(file="{0}card_back_blue@2x.png".
+                                     format(cls.imgdir))
+        return cls.backimg
+
+    @classmethod
+    def card(cls, cardindex=None):
+
+        '''Returns a card image.'''
+
+        if not cls.cardimgs:
+            cls.cardimgs = []
+            for idx in range(0, 52):
+                newcard = PhotoImage(file="{0}card{1}@2x.png".
+                                     format(cls.imgdir, idx))
                 cls.cardimgs.append(newcard)
 
         return cls.cardimgs[cardindex - 1]
